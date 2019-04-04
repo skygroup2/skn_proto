a9e11fe6a0439212b464fe33f1af8d48c235559d
king22 <henrytan.next@gmail.com>
2019-04-04 13:15:54 +0700

    include deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,14 @@ defmodule SknProto.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [
+        :logger,
+        :gun,
+        :cowlib,
+        :idna,
+        :certifi,
+        :ssl_verify_fun
+      ]
     ]
   end
 
