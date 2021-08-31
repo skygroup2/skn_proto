18e07628de6b5249997e581062ec11ce2484744c
king22 <henrytan.next@gmail.com>
2021-08-31 08:23:02 +0700

    fix bug GunEx

--- lib/gun.ex
+++ lib/gun.ex
@@ -114,8 +114,8 @@ defmodule GunEx do
     connect(dhost, port, options, @connect_timeout)
   end
 
-  def connect(dhost, port, options, timeout) do
-    host = if is_binary(dhost), do: :erlang.binary_to_list(dhost), else: dhost
+  def connect(dst_host, port, options, timeout) do
+    host = if is_binary(dst_host), do: :erlang.binary_to_list(dst_host), else: dst_host
     cond do
       options[:socks5_host] != nil ->
         # socks5_host, socks5_port, (socks5_transport), socks5_user, socks5_pass, (socks5_resolve)
@@ -126,22 +126,23 @@ defmodule GunEx do
             exp
         end
       options[:connect_host] != nil and options[:connect_port] != nil ->
-        hostx = options[:connect_host]
-        portx = options[:connect_port]
-        options = List.keyreplace options, :connect_host, 0, {:connect_host, host}
+        proxy_host = options[:connect_host]
+        proxy_port = options[:connect_port]
+        connect_host = if is_tuple(host), do: :inet.ntoa(host), else: host
+        options = List.keyreplace options, :connect_host, 0, {:connect_host, connect_host}
         options = List.keyreplace options, :connect_port, 0, {:connect_port, port}
-        case :gun_http_proxy.connect(hostx, portx, options, timeout) do
+        case :gun_http_proxy.connect(proxy_host, proxy_port, options, timeout) do
           {:ok, {_, socket}} ->
             {:ok, socket}
           exp ->
             exp
         end
       true ->
-        hostx = case :inet.parse_ipv4_address(host) do
+        host_n = case :inet.parse_ipv4_address(host) do
           {:ok, v} -> v
           _ -> host
         end
-        :gen_tcp.connect(hostx, port, options[:tcp_opt], timeout)
+        :gen_tcp.connect(host_n, port, options[:tcp_opt], timeout)
     end
   end
 
